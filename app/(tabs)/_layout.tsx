613fdf53158c49a65a89478c2c9d49cedc421762
Maks <makzylinski@gmail.com>
2026-01-01 22:48:37 +0100

    Added new tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,6 +30,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+       <Tabs.Screen
+        name="log-workout"
+              options={{
+                title: 'Log Workout',
+                tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+              }}
+            />
     </Tabs>
   );
 }
