33183550562987cfea455b92d3d6b2d71cd78719
Maks <makzylinski@gmail.com>
2026-01-02 00:04:31 +0100

    Enhance InputSearch component with state management and styling

--- shared/components/input-search.tsx
+++ shared/components/input-search.tsx
@@ -1,5 +1,26 @@
-import { TextInput } from "react-native";
+import { useState } from "react";
+import { StyleSheet, TextInput } from "react-native";
 
 export default function InputSearch() {
-  return <TextInput placeholder="Search exercise..." />;
+  const [searchText, setSearchText] = useState("");
+
+  return (
+    <TextInput
+      style={styles.input}
+      placeholder="Search exercise..."
+      onChangeText={(text) => setSearchText(text)}
+    />
+  );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    width: 120,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: "white",
+    color: "white",
+  },
+});
