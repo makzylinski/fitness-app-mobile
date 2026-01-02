14e331671d158b5055471e91803613c9d31dba88
Maks <makzylinski@gmail.com>
2026-01-02 13:19:01 +0100

    Refactor InputSearch component to include exercise items display and adjust input width

--- shared/components/input-search.tsx
+++ shared/components/input-search.tsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
-import { StyleSheet, TextInput } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function InputSearch() {
   const [searchText, setSearchText] = useState("");
+  const exerciseItems = ["te", "we"];
 
   return (
-    <TextInput
-      style={styles.input}
-      placeholder="Search exercise..."
-      onChangeText={(text) => setSearchText(text)}
-    />
+    <View>
+      <TextInput
+        style={styles.input}
+        placeholder="Search exercise..."
+        onChangeText={(text) => setSearchText(text)}
+      />
+
+      {exerciseItems.map((exercise, index) => (
+        <Text key={index} style={styles.listItem}>
+          {exercise}
+        </Text>
+      ))}
+    </View>
   );
 }
 
@@ -17,12 +26,20 @@ const styles = StyleSheet.create({
   // TODO: move this to separate file
   input: {
     height: 40,
-    width: 220,
+    width: 250,
     margin: 20,
     alignSelf: "center",
     borderWidth: 1,
     padding: 10,
     borderColor: "white",
     color: "white",
+  },
+  listItem: {
+    width: 250,
+    color: "white",
+    borderWidth: 1,
+    borderColor: "white",
+    alignSelf: "center",
+    padding: 10,
   },
 });
