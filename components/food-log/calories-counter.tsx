a89c4ef2d3286ab2350615f17ce62eba64b1d0a0
Maks <makzylinski@gmail.com>
2026-01-07 17:18:52 +0100

    Add CaloriesCounter component with basic structure and themed styling

---
+++ components/food-log/calories-counter.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "../ui/themed-text";
+import { ThemedView } from "../ui/themed-view";
+
+export default function CaloriesCounter() {
+  return (
+    <ThemedView>
+      <ThemedText>CaloriesCounter works!</ThemedText>
+    </ThemedView>
+  );
+}
