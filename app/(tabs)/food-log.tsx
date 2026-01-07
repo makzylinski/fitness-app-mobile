030ebdcaa942d32e6a6094dcddd8c0b9b7c39e25
Maks <makzylinski@gmail.com>
2026-01-07 17:16:08 +0100

    Refactor FoodDiary component to apply styling and improve layout

--- app/(tabs)/food-log.tsx
+++ app/(tabs)/food-log.tsx
@@ -1,12 +1,19 @@
 import FoodCalendar from "@/components/food-log/food-calendar";
 import Meal from "@/components/food-log/meal";
 import { ThemedView } from "@/components/ui/themed-view";
+import { StyleSheet } from "react-native";
 
 export default function FoodDiary() {
   return (
-    <ThemedView>
+    <ThemedView style={styles.container}>
       <FoodCalendar></FoodCalendar>
       <Meal></Meal>
     </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+  },
+});
