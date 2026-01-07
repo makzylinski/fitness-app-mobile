ede5a1265393d5173ed852dcc9d1946f60b90438
Maks <makzylinski@gmail.com>
2026-01-07 15:38:52 +0100

    Refactor Meal component to display meal details with dynamic background color

--- components/food-log/meal.tsx
+++ components/food-log/meal.tsx
@@ -1,11 +1,26 @@
+import { useThemeColor } from "@/hooks/use-theme-color";
 import { StyleSheet } from "react-native";
 import { ThemedText } from "../ui/themed-text";
 import { ThemedView } from "../ui/themed-view";
 
 export default function Meal() {
+  const backgroundColor = useThemeColor({}, "inputBackground");
   return (
     <ThemedView style={styles.container}>
-      <ThemedText>Elo</ThemedText>
+      <ThemedView style={[{ backgroundColor }, styles.card]}>
+        <ThemedView style={styles.info}>
+          <ThemedView>
+            <ThemedText>Breakfast</ThemedText>
+            <ThemedText>8:30</ThemedText>
+          </ThemedView>
+
+          <ThemedView>
+            <ThemedText style={styles.kalories}>560</ThemedText>
+            <ThemedText style={styles.macros}>P: 24g F: 17g C: 68g</ThemedText>
+          </ThemedView>
+        </ThemedView>
+        <ThemedView style={styles.action}></ThemedView>
+      </ThemedView>
     </ThemedView>
   );
 }
@@ -14,4 +29,12 @@ const styles = StyleSheet.create({
   container: {
     margin: 20,
   },
+  card: {
+    padding: 20,
+    borderRadius: 10,
+  },
+  info: {},
+  action: {},
+  kalories: {},
+  macros: {},
 });
