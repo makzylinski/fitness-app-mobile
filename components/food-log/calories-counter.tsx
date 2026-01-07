bb6e9e7b052eeb03803a7599b4a2e8b13c23ac22
Maks <makzylinski@gmail.com>
2026-01-07 17:24:56 +0100

    Add CaloriesCounter component to FoodDiary and apply styling

--- components/food-log/calories-counter.tsx
+++ components/food-log/calories-counter.tsx
@@ -1,10 +1,21 @@
+import { StyleSheet } from "react-native";
 import { ThemedText } from "../ui/themed-text";
 import { ThemedView } from "../ui/themed-view";
 
 export default function CaloriesCounter() {
   return (
-    <ThemedView>
+    <ThemedView style={styles.container}>
       <ThemedText>CaloriesCounter works!</ThemedText>
     </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10,
+  },
+});
