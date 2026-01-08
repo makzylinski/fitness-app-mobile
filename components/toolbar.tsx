438bc6fb9919ca86a2c4fe8cfa50db4a3aef64e4
Maks <makzylinski@gmail.com>
2026-01-08 16:31:21 +0100

    Add Toolbar component and update package configurations

---
+++ components/toolbar.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "./ui/themed-text";
+import { ThemedView } from "./ui/themed-view";
+
+export default function Toolbar() {
+  return (
+    <ThemedView>
+      <ThemedText>Toolbar works!</ThemedText>
+    </ThemedView>
+  );
+}
