562900a746fa93b8749bce52307eacc170a8381d
Maks <makzylinski@gmail.com>
2026-01-08 13:53:43 +0100

    Refactor MealDetails component to enhance styling and integrate theme color

--- components/food-log/meal-details.tsx
+++ components/food-log/meal-details.tsx
@@ -1,3 +1,4 @@
+import { useThemeColor } from "@/hooks/use-theme-color";
 import { StyleSheet } from "react-native";
 import { ThemedText } from "../ui/themed-text";
 import { ThemedView } from "../ui/themed-view";
@@ -16,9 +17,12 @@ type MealDetailsProps = {
 };
 
 export default function MealDetails({ product }: MealDetailsProps) {
+  const backgroundColor = useThemeColor({}, "inputBackground");
+
   return (
     <ThemedView style={styles.container}>
-      <ThemedView style={styles.card}>
+      <ThemedView style={styles.containerLine}> </ThemedView>
+      <ThemedView style={[{ backgroundColor }, styles.card]}>
         <ThemedText>{product.name}</ThemedText>
       </ThemedView>
     </ThemedView>
@@ -26,6 +30,19 @@ export default function MealDetails({ product }: MealDetailsProps) {
 }
 
 const styles = StyleSheet.create({
-  container: {},
-  card: {},
+  container: {
+    flexDirection: "row",
+    gap: 10,
+  },
+  containerLine: {
+    backgroundColor: "#222222",
+    width: 3,
+    marginLeft: 10,
+  },
+  card: {
+    backgroundColor: "#222222",
+    padding: 10,
+    width: "94%",
+    borderRadius: 10,
+  },
 });
