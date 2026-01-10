18b6e1969495dd59c51688752e105e3578e6da39
Maks <makzylinski@gmail.com>
2026-01-10 22:07:07 +0100

    Rename HomeScreen to Dashboard and create a new Dashboard component

---
+++ app/(tabs)/dashboard.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "../../components/ui/themed-text";
+import { ThemedView } from "../../components/ui/themed-view";
+
+export default function Dashboard() {
+  return (
+    <ThemedView>
+      <ThemedText>Dashboard works!</ThemedText>
+    </ThemedView>
+  );
+}
