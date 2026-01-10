330fbf67bbe69144c76bef1c3bc5048c9a984f93
Maks <makzylinski@gmail.com>
2026-01-10 22:16:23 +0100

    Add Dashboard component and integrate CalorieSummary

--- components/dashboard/calorie-summary.tsx
+++ components/dashboard/calorie-summary.tsx
@@ -1,11 +1,17 @@
+import { useThemeColor } from "@/hooks/use-theme-color";
 import { StyleSheet } from "react-native";
 import { ThemedText } from "../ui/themed-text";
 import { ThemedView } from "../ui/themed-view";
 
 export default function CalorieSummary() {
+  const backgroundColor = useThemeColor({}, "inputBackground");
+
   return (
     <ThemedView>
       <ThemedText style={styles.header}>Today&apos;s Clorie Summary</ThemedText>
+      <ThemedView style={[{ backgroundColor }, styles.container]}>
+        <ThemedText>Elo</ThemedText>
+      </ThemedView>
     </ThemedView>
   );
 }
@@ -14,5 +20,13 @@ const styles = StyleSheet.create({
   header: {
     fontWeight: 700,
     fontSize: 24,
+  },
+  container: {
+    padding: 20,
+    marginVertical: 10,
+    borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 });
