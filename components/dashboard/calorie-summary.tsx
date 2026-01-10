6098888a67fb1601d2414b5b368def474b5aa228
Maks <makzylinski@gmail.com>
2026-01-10 22:13:11 +0100

    Add styling to CalorieSummary component header

--- components/dashboard/calorie-summary.tsx
+++ components/dashboard/calorie-summary.tsx
@@ -1,10 +1,18 @@
+import { StyleSheet } from "react-native";
 import { ThemedText } from "../ui/themed-text";
 import { ThemedView } from "../ui/themed-view";
 
 export default function CalorieSummary() {
   return (
     <ThemedView>
-      <ThemedText>Today&apos;s Clorie Summary</ThemedText>
+      <ThemedText style={styles.header}>Today&apos;s Clorie Summary</ThemedText>
     </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    fontWeight: 700,
+    fontSize: 24,
+  },
+});
