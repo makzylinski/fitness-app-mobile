8cdcb54624c9b67f11f73f6dfee349f0ffea63a9
Maks <makzylinski@gmail.com>
2026-01-11 10:27:41 +0100

    Add RecentWorkouts component to display recent workout information

---
+++ components/dashboard/recent-workouts.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "../ui/themed-text";
+import { ThemedView } from "../ui/themed-view";
+
+export default function RecentWorkouts() {
+  return (
+    <ThemedView>
+      <ThemedText>Recent Workouts work!</ThemedText>
+    </ThemedView>
+  );
+}
