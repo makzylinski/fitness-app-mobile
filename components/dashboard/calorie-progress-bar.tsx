9bd30695b552657f0c3824f9d951baff565e5359
Maks <makzylinski@gmail.com>
2026-01-11 07:51:41 +0100

    Adjust CalorieProgressBar width and align items in CalorieSummary's caloriesSection

--- components/dashboard/calorie-progress-bar.tsx
+++ components/dashboard/calorie-progress-bar.tsx
@@ -28,7 +28,7 @@ export default function CalorieProgressBar() {
 
 const styles = StyleSheet.create({
   container: {
-    height: "100%",
+    width: "60%",
   },
   caloriesConsumed: {},
   consumed: {
--- components/dashboard/calorie-summary.tsx
+++ components/dashboard/calorie-summary.tsx
@@ -36,6 +36,7 @@ const styles = StyleSheet.create({
   },
   caloriesSection: {
     flexDirection: "row",
+    alignItems: "center",
     justifyContent: "space-between",
   },
 });
