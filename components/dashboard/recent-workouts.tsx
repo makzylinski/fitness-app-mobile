687b7bcc3761cf23482dc6d559c3ba799dc11136
Maks <makzylinski@gmail.com>
2026-01-11 11:58:52 +0100

    Add SingleRecentWorkout component and integrate it into RecentWorkouts for improved workout display

--- components/dashboard/recent-workouts.tsx
+++ components/dashboard/recent-workouts.tsx
@@ -3,9 +3,18 @@ import { StyleSheet } from "react-native";
 import ThemedCard from "../ui/themed-card";
 import { ThemedText } from "../ui/themed-text";
 import { ThemedView } from "../ui/themed-view";
+import SingleRecentWorkout from "./single-recent-workout";
 
 export default function RecentWorkouts() {
   const primaryColor = useThemeColor({}, "primaryColor");
+
+  const recentWorkouts = [
+    {
+      name: "Upper Body Power",
+      date: new Date(),
+      duration: "45min",
+    },
+  ];
 
   return (
     <ThemedView>
@@ -13,7 +22,16 @@ export default function RecentWorkouts() {
         <ThemedText style={styles.header}>Recent Workouts</ThemedText>
         <ThemedText style={{ color: primaryColor }}>View All</ThemedText>
       </ThemedView>
-      <ThemedCard style={styles.container}></ThemedCard>
+      <ThemedCard style={styles.container}>
+        {recentWorkouts.map((workout, index) => (
+          <SingleRecentWorkout
+            key={index}
+            name={workout.name}
+            date={workout.date}
+            duration={workout.duration}
+          />
+        ))}
+      </ThemedCard>
     </ThemedView>
   );
 }
