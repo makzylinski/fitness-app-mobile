39d48cecaa935e54d1bc64ab0480f1d6d138bf5e
Maks <makzylinski@gmail.com>
2026-01-11 08:01:27 +0100

    Refactor CalorieProgressBar and ProgressBar components for improved styling and structure

--- components/dashboard/calorie-progress-bar.tsx
+++ components/dashboard/calorie-progress-bar.tsx
@@ -10,7 +10,7 @@ export default function CalorieProgressBar() {
 
   return (
     <ThemedView style={[{ backgroundColor }, styles.container]}>
-      <ThemedView style={styles.caloriesConsumed}>
+      <ThemedView style={[{ backgroundColor }, styles.caloriesConsumed]}>
         <ThemedView style={[{ backgroundColor }, styles.counter]}>
           <ThemedText style={styles.consumed}>1450</ThemedText>
           <ThemedText style={[{ color }, styles.calorieLimit]}>
--- shared/components/progress-bar.tsx
+++ shared/components/progress-bar.tsx
@@ -1,17 +1,32 @@
-import { ThemedText } from "@/components/ui/themed-text";
 import { ThemedView } from "@/components/ui/themed-view";
 import { useThemeColor } from "@/hooks/use-theme-color";
 import { StyleSheet } from "react-native";
 
 export default function ProgressBar() {
-  const backgroundColor = useThemeColor({}, "inputBackground");
+  const primaryColor = useThemeColor({}, "primaryColor");
 
   return (
-    <ThemedView style={[{ backgroundColor }, styles.container]}>
-      <ThemedText>progress bar</ThemedText>
+    <ThemedView style={styles.container}>
+      <ThemedView
+        style={[
+          styles.kcalProgress,
+          { backgroundColor: primaryColor, width: "54%" },
+        ]}
+      />
     </ThemedView>
   );
 }
 const styles = StyleSheet.create({
-  container: {},
+  wrapper: {},
+  container: {
+    width: "100%",
+    height: 10,
+    marginVertical: 5,
+    borderRadius: 10,
+    backgroundColor: "#3A3A3C",
+  },
+  kcalProgress: {
+    height: "100%",
+    borderRadius: 10,
+  },
 });
