81578b79a1a99c8728041579496b3c289047622a
Maks <makzylinski@gmail.com>
2026-01-12 13:31:11 +0100

    Refactor Login component for improved layout and styling

--- app/login.tsx
+++ app/login.tsx
@@ -1,15 +1,21 @@
+import { useThemeColor } from "@/hooks/use-theme-color";
+import { StyleSheet } from "react-native";
 import ThemedButton from "../components/ui/themed-button";
 import { ThemedInput } from "../components/ui/themed-input";
 import { ThemedText } from "../components/ui/themed-text";
 import { ThemedView } from "../components/ui/themed-view";
 
 export default function Login() {
+  const color = useThemeColor({}, "inputLabel");
+
   return (
-    <ThemedView>
-      <ThemedView>
+    <ThemedView style={styles.container}>
+      <ThemedView style={styles.headerContainer}>
         <ThemedText>icon</ThemedText>
-        <ThemedText>name</ThemedText>
-        <ThemedText>subheader</ThemedText>
+        <ThemedText style={styles.header}>name</ThemedText>
+        <ThemedText style={[{ color }, styles.subheader]}>
+          Strength & Nutrition
+        </ThemedText>
       </ThemedView>
       <ThemedView>
         <ThemedInput></ThemedInput>
@@ -23,3 +29,20 @@ export default function Login() {
     </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 20,
+  },
+  headerContainer: {
+    alignItems: "center",
+    marginVertical: 30,
+  },
+  header: {
+    textTransform: "uppercase",
+    fontWeight: 700,
+    fontSize: 24,
+    paddingBottom: 12,
+  },
+  subheader: {},
+});
