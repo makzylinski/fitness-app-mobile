bb6de1988a57fc4a319b316222416bb6623432a6
Maks <makzylinski@gmail.com>
2026-01-13 23:54:25 +0100

    Implement sign-up form with input fields and password visibility toggle

--- app/sign-up.tsx
+++ app/sign-up.tsx
@@ -1,12 +1,81 @@
+import { ThemedInput } from "@/components/ui/themed-input";
 import { ThemedText } from "@/components/ui/themed-text";
 import { ThemedView } from "@/components/ui/themed-view";
-import { StyleSheet } from "react-native";
+import { useThemeColor } from "@/hooks/use-theme-color";
+import { Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
+import { Pressable, StyleSheet } from "react-native";
 
 export default function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const color = useThemeColor({}, "inputLabel");
   return (
     <ThemedView style={styles.container}>
       <ThemedView style={styles.headerContainer}>
         <ThemedText style={styles.header}>Create Account</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.formContainer}>
+        <ThemedInput
+          style={styles.input}
+          placeholder="Joe Doe"
+          value={name}
+          label="Name"
+          onChangeText={(text) => setName(text)}
+        />
+        <ThemedInput
+          style={styles.input}
+          placeholder="example@test.com"
+          value={email}
+          label="Email Address"
+          onChangeText={(text) => setEmail(text)}
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+
+        <ThemedInput
+          style={styles.input}
+          placeholder="Enter your password"
+          value={password}
+          label="Password"
+          secureTextEntry={!showPassword}
+          onChangeText={(text) => setPassword(text)}
+          rightIcon={
+            <Pressable
+              onPress={() => setShowPassword(!showPassword)}
+              style={{ paddingBottom: 14 }}
+            >
+              <Ionicons
+                name={showPassword ? "eye-off" : "eye"}
+                size={20}
+                color={color}
+              />
+            </Pressable>
+          }
+        />
+        <ThemedInput
+          style={styles.input}
+          placeholder="Repeat your Password"
+          value={password}
+          label="Confirm Password"
+          secureTextEntry={!showPassword}
+          onChangeText={(text) => setPassword(text)}
+          rightIcon={
+            <Pressable
+              onPress={() => setShowPassword(!showPassword)}
+              style={{ paddingBottom: 14 }}
+            >
+              <Ionicons
+                name={showPassword ? "eye-off" : "eye"}
+                size={20}
+                color={color}
+              />
+            </Pressable>
+          }
+        />
       </ThemedView>
     </ThemedView>
   );
@@ -29,5 +98,14 @@ const styles = StyleSheet.create({
     fontSize: 26,
     letterSpacing: 2,
     marginBottom: 8,
+  },
+  formContainer: {
+    flex: 1,
+  },
+  input: {
+    borderRadius: 16,
+    padding: 16,
+    fontSize: 16,
+    marginBottom: 20,
   },
 });
