ca29ec66e2dc7f603f0cc51d24cecb70f205f40b
Maks <makzylinski@gmail.com>
2026-01-22 22:45:54 +0100

    Add TimerContext for managing timer state in the application

---
+++ shared/contexts/TimerContext.tsx
@@ -0,0 +1,19 @@
+import { createContext, ReactNode, useState } from "react";
+
+type TimerType = {
+  time: number;
+  increment: () => void;
+};
+
+const TimerContext = createContext<TimerType | undefined>(undefined);
+
+export const TimerProvider = ({ children }: { children: ReactNode }) => {
+  const [time, setTime] = useState(0);
+  const increment = () => setTime((t) => t + 1);
+
+  return (
+    <TimerContext.Provider value={{ time, increment }}>
+      {children}
+    </TimerContext.Provider>
+  );
+};
