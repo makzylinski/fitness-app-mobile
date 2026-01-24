6dc472a948fa8291e5a802dfb8308d4667f72630
Maks <makzylinski@gmail.com>
2026-01-24 13:18:31 +0100

    Integrate ActiveWorkoutWidget into RootLayout and enhance styling with theme colors

--- components/new-workout/active-workout-widget.tsx
+++ components/new-workout/active-workout-widget.tsx
@@ -1,15 +1,27 @@
+import { useThemeColor } from "@/hooks/use-theme-color";
 import { StyleSheet } from "react-native";
 import { ThemedText } from "../ui/themed-text";
 import { ThemedView } from "../ui/themed-view";
 
 export default function ActiveWorkoutWidget() {
+  const primaryColor = useThemeColor({}, "primaryColor");
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText>ActiveWorkoutWidget works!</ThemedText>
+    <ThemedView style={[{ backgroundColor: primaryColor }, styles.container]}>
+      <ThemedText>Push Day Routine</ThemedText>
+      <ThemedText>Elapsed: 42:15</ThemedText>
     </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    position: "absolute",
+    bottom: 60,
+    color: "white",
+    zIndex: 100,
+    width: "90%",
+    padding: 10,
+    borderRadius: 12,
+    alignSelf: "center",
+  },
 });
