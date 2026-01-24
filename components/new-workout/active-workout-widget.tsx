21109e600b39c3e45c5a7e73e1292c8dadc5fa43
Maks <makzylinski@gmail.com>
2026-01-24 13:12:07 +0100

    Refactor ActiveWorkoutWidget to apply styles to ThemedView component

--- components/new-workout/active-workout-widget.tsx
+++ components/new-workout/active-workout-widget.tsx
@@ -1,10 +1,15 @@
+import { StyleSheet } from "react-native";
 import { ThemedText } from "../ui/themed-text";
 import { ThemedView } from "../ui/themed-view";
 
 export default function ActiveWorkoutWidget() {
   return (
-    <ThemedView>
+    <ThemedView style={styles.container}>
       <ThemedText>ActiveWorkoutWidget works!</ThemedText>
     </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {},
+});
