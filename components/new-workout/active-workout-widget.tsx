3788fd4a1100655b19fdae25e4955f6376e148f0
Maks <makzylinski@gmail.com>
2026-01-24 13:11:27 +0100

    Add ActiveWorkoutWidget component with basic structure

---
+++ components/new-workout/active-workout-widget.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "../ui/themed-text";
+import { ThemedView } from "../ui/themed-view";
+
+export default function ActiveWorkoutWidget() {
+  return (
+    <ThemedView>
+      <ThemedText>ActiveWorkoutWidget works!</ThemedText>
+    </ThemedView>
+  );
+}
