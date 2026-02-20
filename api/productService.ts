29f52714e6b994d6299fdc581703c82f93018f7f
Maks <makzylinski@gmail.com>
2026-02-20 15:56:00 +0100

    Fix fetchProductByBarcode to send barcode as an object

--- api/productService.ts
+++ api/productService.ts
@@ -1,5 +1,5 @@
 import apiClient from "./apiClient";
 
 export const fetchProductByBarcode = async (code: string) => {
-  await apiClient.post("/products/lookup", "3017624010701");
+  await apiClient.post("/products/lookup", { barcode: "3017624010701" }); // test data
 };
