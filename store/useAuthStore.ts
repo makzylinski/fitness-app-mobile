6f66cd5a24ee7d5b307504dfb9736c69157c38be
Maks <makzylinski@gmail.com>
2026-02-25 18:09:40 +0100

    Refactor useAuthStore to enforce User type for authentication

--- store/useAuthStore.ts
+++ store/useAuthStore.ts
@@ -1,13 +1,18 @@
 import { create } from "zustand";
 
+type User = {
+  // TODO change it and export
+  email: string;
+};
+
 type AuthState = {
-  user: any;
+  user: User | null;
   login: (name: any) => void;
   logout: () => void;
 };
 
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
-  login: (userObj) => set({ user: userObj }),
+  login: (userObj: User) => set({ user: userObj }),
   logout: () => set({ user: null }),
 }));
