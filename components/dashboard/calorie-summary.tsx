dc977d309317bc85de496d89bf6f32c2f7da3216
Maks <makzylinski@gmail.com>
2026-03-08 22:25:16 +0100

    Refactor CalorieSummary component to use custom hook for calorie summary management and create useCalorieSummary hook

--- components/dashboard/calorie-summary.tsx
+++ components/dashboard/calorie-summary.tsx
@@ -1,4 +1,5 @@
 import { useThemeColor } from "@/hooks/use-theme-color";
+import { UseCalorieSummary } from "@/hooks/useCalorieSummary";
 import PieChart from "@/shared/components/pie-chart";
 import { StyleSheet } from "react-native";
 import ThemedButton from "../ui/themed-button";
@@ -10,33 +11,9 @@ import MacrosSection from "./macros-section";
 
 export default function CalorieSummary() {
   const backgroundColor = useThemeColor("inputBackground");
-  const accent = useThemeColor("accent");
-  const primaryColor = useThemeColor("primaryColor");
-  const secondaryColor = useThemeColor("secondaryColor");
   const borderBottomColor = useThemeColor("inputBorder");
 
-  const macrosData = [
-    {
-      name: "protein",
-      color: `${primaryColor}`,
-      actualValue: 100,
-      maxValue: 180,
-    },
-    {
-      name: "carbs",
-      color: `${accent}`,
-      actualValue: 80,
-      maxValue: 260,
-    },
-    {
-      name: "fats",
-      color: `${secondaryColor}`,
-      actualValue: 220,
-      maxValue: 320,
-    },
-  ];
-
-  const onLogMeal = () => console.log("log meal");
+  const { onLogMeal, macrosData } = UseCalorieSummary();
 
   return (
     <ThemedView>
